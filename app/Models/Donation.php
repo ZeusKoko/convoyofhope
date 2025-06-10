ba9efaba8c13117b517593b9f935ec95f1be4716
vic657 <jumav1211@gmail.com>
2025-06-11 01:25:39 +0300

    user dashboard done

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Donation extends Model
 {
-    protected $fillable = ['name', 'email', 'national_id', 'nationality', 'event_id', 'amount'];
+    protected $fillable = ['name', 'email', 'national_id', 'nationality', 'event_id', 'amount','user_id'];
 
+
+public function user()
+{
+    return $this->belongsTo(User::class);
 }
+
+public function event()
+{
+    return $this->belongsTo(Event::class);
+}
+}
+
